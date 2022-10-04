32f0722065ea23308486422d1ecf7eecefc128eb
Gleb Shamey <wwow230@gmail.com>
2022-10-04 16:45:47 +0700

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import { Product } from './components/Product'
+import axios from 'axios'
+import {IProduct} from './models'
+
+function App() {
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+    setProducts(response.data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  return (
+     <div className='container mx-auto max-w-2xl pt-5'>
+      { products.map(product => <Product product={product} key={product.id} />) }
+     </div>
+  )
+}
+
+export default App
